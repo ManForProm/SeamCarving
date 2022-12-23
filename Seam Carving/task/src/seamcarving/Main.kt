e4587d14e4ff6e9a67f02a97b45c9d5a6749561a
yahor <manforprom@gmail.com>
2022-12-23 13:30:58 +0300

    implemented energy calculating

--- Seam Carving/task/src/seamcarving/Main.kt
+++ Seam Carving/task/src/seamcarving/Main.kt
@@ -30,49 +30,38 @@ fun parseArgs(args: Array<String>): ParcedArgsData {
 }
 
 data class ParcedArgsData(val loadedImage: BufferedImage, val saveImagePath: String)
-
-enum class RGB(val color: Color.() -> Int) { RED({ this.red }), GREEN({ this.green }), BLUE({ this.blue }) }
+enum class RGB(val colorFunction: Color.() -> Int) { RED({ this.red }), GREEN({ this.green }), BLUE({ this.blue }) }
 
 class Picture(val imageIn: BufferedImage) {
     private val height = imageIn.height
     private val width = imageIn.width
-
-//    private val loadeSave = LoadSaveImage()
-//    fun createNewPicture(pathSave:String){
-//        imageIn.findEnergy()
-//        loadeSave.saveImagePng(imageIn,File(pathSave))
-//    }
+    private var maxPixelEnergy = 0.0
+    private val energyMatrix = List(imageIn.width) { MutableList(imageIn.height) { 0.0 } }
 
     fun replacePictureWithEnergy() {
-        val maxPixelEnergy = imageIn.findMaxEnergy()
-        imageIn.doForEveryPixel { posX, posY, color ->
-            val pixelEnergy = calculateEnergy(posX, posY)
-            val intensity = (255.0 * pixelEnergy / maxPixelEnergy).toInt()
+
+        imageIn.doForEveryPixel { posX, posY, _ ->
+            val pixelEnergy = calculateEnergyPixel(posX, posY)
+            maxPixelEnergy = max(pixelEnergy, maxPixelEnergy)
+            energyMatrix[posX][posY] = pixelEnergy
+        }
+        imageIn.doForEveryPixel { posX, posY, _ ->
+            val intensity = (255.0 * energyMatrix[posX][posY] / maxPixelEnergy).toInt()
             imageIn.setRGB(posX, posY, Color(intensity, intensity, intensity).rgb)
         }
     }
-
-    private fun BufferedImage.findMaxEnergy(): Double {
-        var maxPixelEnergy = 0.0
-        this.doForEveryPixel { posX, posY, color ->
-            val pixelEnergy = calculateEnergy(posX, posY)
-            if (pixelEnergy > maxPixelEnergy) maxPixelEnergy = pixelEnergy
-        }
-        return maxPixelEnergy
-    }
-
-    private fun calculateEnergy(posX: Int, posY: Int): Double {
-        getEnergyPositions(getEnergyPosPair(posX, posY, height - 1, width - 1)).apply {
+    private fun calculateEnergyPixel(posX: Int, posY: Int): Double {
+        getEnergyPositions(posX, posY).apply {
             return calculateEnergyFormula(
-                leftPixelX = mapToColor("x1"),
-                rightPixelX = mapToColor("x2"),
-                abovePixelY = mapToColor("y1"),
-                belowPixelY = mapToColor("y2")
+                leftPixelX = energyPositionMapToColor("x1"),
+                rightPixelX = energyPositionMapToColor("x2"),
+                abovePixelY = energyPositionMapToColor("y1"),
+                belowPixelY = energyPositionMapToColor("y2")
             )
         }
     }
 
-    private fun Map<String, Pair<Int, Int>>.mapToColor(coordKey: String): Color {
+    private fun Map<String, Pair<Int, Int>>.energyPositionMapToColor(coordKey: String): Color {
         try {
             return Color(imageIn.getRGB(this[coordKey]!!.first, this[coordKey]!!.second))
         } catch (e: Exception) {
@@ -80,7 +69,6 @@ class Picture(val imageIn: BufferedImage) {
         }
         return Color(0)
     }
-
     private fun calculateEnergyFormula(
         leftPixelX: Color,
         rightPixelX: Color,
@@ -88,7 +76,6 @@ class Picture(val imageIn: BufferedImage) {
         belowPixelY: Color
     ): Double {
         val xGradient = gradientCalculation(leftPixelX, rightPixelX, RGB.RED, RGB.GREEN, RGB.BLUE)
-
         val yGradient = gradientCalculation(abovePixelY, belowPixelY, RGB.RED, RGB.GREEN, RGB.BLUE)
         return sqrt(xGradient + yGradient)
     }
@@ -101,62 +88,40 @@ class Picture(val imageIn: BufferedImage) {
         return sum
     }
 
-    private fun minusPixelToDoublePow(firstPixel: Color, secondPixel: Color, color: RGB) =
-//            (firstPixel.getPixelColorEasy(color) - secondPixel.getPixelColorEasy(color)).toDouble().pow(2)
-        when (color) {
-            RGB.RED -> (secondPixel.red - firstPixel.red).toDouble().pow(2)
-            RGB.BLUE -> (secondPixel.blue - firstPixel.blue).toDouble().pow(2)
-            RGB.GREEN -> (secondPixel.green - firstPixel.green).toDouble().pow(2)
-        }
+    private fun minusPixelToDoublePow(firstPixel: Color, secondPixel: Color, color: RGB): Double {
+        return (abs(
+            firstPixel.getPixelColorEasy(color).toDouble() - secondPixel.getPixelColorEasy(color).toDouble()
+        )).pow(2)
+    }
 
     private fun Color.getPixelColorEasy(color: RGB): Int {
-        val colorFunction = color.color
+        val colorFunction = color.colorFunction
         return this.colorFunction()
     }
 
-    private fun getEnergyPositions(map: Map<String, Pair<Int, Int>>): Map<String, Pair<Int, Int>> {
-        return mapOf(
-            "x1" to Pair(map.getXPairX() - 1, map.getYPairX()),//  left pair around x pixel
-            "x2" to Pair(map.getXPairX() + 1, map.getYPairX()),// right pair around x pixel
-            "y1" to Pair(map.getXPairY(), map.getYPairY() - 1),// top pair around y pixel
-            "y2" to Pair(map.getXPairY(), map.getYPairY() + 1)// bottom pair around y pixel
-        )
-    }
+    private fun getEnergyPositions(posX: Int, posY: Int): Map<String, Pair<Int, Int>> {
+        getEnergyPosPair(posX, posY).apply {
+            return mapOf(
+                "x1" to Pair(first + 1, posY),//  left pair around x pixel
+                "x2" to Pair(first - 1, posY),// right pair around x pixel
+                "y1" to Pair(posX, second + 1),// top pair around y pixel
+                "y2" to Pair(posX, second - 1)// bottom pair around y pixel
+            )
+        }
 
-    fun Map<String, Pair<Int, Int>>.getXPairX() = this.get("x1")!!.first
-    fun Map<String, Pair<Int, Int>>.getYPairX() = this.get("x1")!!.second
-    fun Map<String, Pair<Int, Int>>.getXPairY() = this.get("y1")!!.first
-    fun Map<String, Pair<Int, Int>>.getYPairY() = this.get("y1")!!.second
+    }
 
     //get our position of pixel depends on border or not
-    private fun getEnergyPosPair(posX: Int, posY: Int,height: Int,width: Int): Map<String, Pair<Int, Int>> {
-        if (posX == 0 && posY > 0 && posY != height) { // x left border y not top and not bottom
-            return positionToMap(posX + 1, posY, posX, posY)
-        } else if (posY == 0 && posX > 0 && posX != width) { // y top x not left not right
-            return positionToMap(posX, posY, posX, posY + 1)
-        } else if (posX == 0 && posY == 0) { // x left y top
-            return positionToMap(posX + 1, posY + 1, posX + 1, posY + 1)
-        } else if (posX == width && posY != height && posY != 0) {// x right and y not top and not bottom
-            return positionToMap(posX - 1, posY, posX, posY)
-        } else if (posY == height && posX != width && posX != 0) { // y bottom and x not left right
-            return positionToMap(posX, posY, posX, posY - 1)
-        } else if (posX == width && posY == height) { //x right y bottom
-            return positionToMap(posX - 1, posY - 1, posX - 1, posY - 1)
-        } else if (posX == 0 && posY == height) {// x left y bottom
-            return positionToMap(posX + 1, posY - 1, posX + 1, posY - 1)
-        } else if (posX == width && posY == 0) { //x right y top
-            return positionToMap(posX - 1, posY + 1, posX - 1, posY + 1)
-        } else return positionToMap(posX, posY, posX, posY) // x not left right y not top bottom
+    private fun getEnergyPosPair(posX: Int, posY: Int): Pair<Int, Int> {
+        val xNew = if (posX == 0) 1 else if (posX == width - 1) posX - 1 else posX
+        val yNew = if (posY == 0) 1 else if (posY == height - 1) posY - 1 else posY
+        return Pair(xNew, yNew)
     }
 
-    private fun positionToMap(xPosX: Int, xPosY: Int, yPosX: Int, yPosY: Int) =
-//        mapOf("x1" to pairX,"y1" to pairY)
-        mapOf("x1" to Pair(xPosX, xPosY), "y1" to Pair(yPosX, yPosY))
-
-    fun BufferedImage.doNegative() {
-        this.doForEveryPixel { posX, posY, color ->
+    fun doNegative() {
+        imageIn.doForEveryPixel { posX, posY, color ->
             val newColor = Color(255 - color.red, 255 - color.green, 255 - color.blue)
-            this.setRGB(posX, posY, newColor.rgb)
+            imageIn.setRGB(posX, posY, newColor.rgb)
         }
     }
 
@@ -167,8 +132,8 @@ class Picture(val imageIn: BufferedImage) {
 
 
 fun BufferedImage.doForEveryPixel(action: (posX: Int, posY: Int, color: Color) -> Unit) {
-    for (x in 0 until this.width) {
-        for (y in 0 until this.height) {
+    for (x in this.minX until this.width) {
+        for (y in this.minY until this.height) {
             action(x, y, Color(this.getRGB(x, y)))
         }
     }
